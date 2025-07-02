637d61428a16777de2cf17cb9fdcb19a7b972bc8
Tushar Agarwal <118683291+TusharAgarwal12@users.noreply.github.com>
2025-07-03 02:27:32 +0530

    Update MLPro_IrisFlowerClassification.py

--- MLPro_IrisFlowerClassification.py
+++ MLPro_IrisFlowerClassification.py
@@ -43,36 +43,3 @@ print("Accuracy:", accuracy_score(y_test, y_pred))
 print("Classification Report:\n", classification_report(y_test, y_pred))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
